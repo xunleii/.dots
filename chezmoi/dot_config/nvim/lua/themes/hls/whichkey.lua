f7e14c3fd3db5c1800b1d71e362e2656f257d58b
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2024-04-04 10:22:34 +0200

    :fire: remove current nvim configuration

--- chezmoi/dot_config/nvim/lua/themes/hls/whichkey.lua
+++
@@ -1,9 +0,0 @@
-return function(colors)
-  return {
-    WhichKey = { fg = colors.color4 },
-    WhichKeySeparator = { fg = colors.color8 },
-    WhichKeyDesc = { fg = colors.color1 },
-    WhichKeyGroup = { fg = colors.color3 },
-    WhichKeyValue = { fg = colors.color5 },
-  }
-end
